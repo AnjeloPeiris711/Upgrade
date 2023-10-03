0404618a39ad349d151082e26725b0d86a4e2465
Anjelo <benjunior711@gmail.com>
2023-10-03 10:16:45 +0530

    Update Ui

--- Upgrade/src/components/App.js
+++ Upgrade/src/components/App.js
@@ -1,24 +1,48 @@
-import React,{Component} from "react";
+import React,{Component,useState} from "react";
+import { Icon } from '@iconify/react';
 import './App.css'
-class App extends Component{
-    render(){
+
+function App() {
+    const [isActivatedRecord, setIsActivatedRecord] = useState(false);
+    const [isActivatedClear, setIsActivatedClear] = useState(false);
+    const [isActivatedFilter, setIsActivatedFilter] = useState(false);
+    const [isActivatedSearch, setIsActivatedSearch] = useState(false);
+
+    const handleRecordClick = () => {
+        setIsActivatedRecord(!isActivatedRecord);
+    };
+    const handleClearClick = () => {
+        setIsActivatedClear(!isActivatedClear);
+    };
+    const handleFilterClick = () => {
+        setIsActivatedFilter(!isActivatedFilter);
+    };
+    const handleSearchClick = () => {
+        setIsActivatedSearch(!isActivatedSearch);
+    };
         return(
             <div className="container">
                 <div className="topnav">
-                    <a className="active" href="#record">record</a>
-                    <a href="#news">News</a>
-                    <a href="#contact">Contact</a>
-                    <a href="#about">About</a>
+                <a href="#record" onClick={handleRecordClick}>
+                    {/* <Icon icon={`mdi:power${isActivated ? '-off' : ''}`} /> */}
+                    <Icon className={`ricon ${isActivatedRecord ? 'ractive-icon' : ''}`} icon={`mdi:power`} />
+                </a>
+                    <a href="#clear" onClick={handleClearClick}>
+                    <Icon className={`cicon ${isActivatedClear ? 'cactive-icon' : ''}`} icon={`mdi:clear-outline`} />
+                    </a>
+                    <a href="#filter" onClick={handleFilterClick}>
+                    <Icon className={`ficon ${isActivatedFilter ? 'factive-icon' : ''}`} icon={`mdi:filter-outline`} />
+                    </a>
+                    <a href="#search" onClick={handleSearchClick}>
+                    <Icon className={`sicon ${isActivatedSearch ? 'sactive-icon' : ''}`} icon={`mdi:search`} />
+                    </a>
                 </div>
                 <main>
                     <p>Welcome to the Upgrade Mode interface.</p>
                     <button className="upgrade-button">Activate</button>
                 </main>
-        <footer>
-            <p>&copy; 2023 Ben 10</p>
-        </footer>
     </div>
+
         )
     }
-}
 export default App;
